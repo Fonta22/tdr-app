87590d9d31d2d458e9c8f11ae365c8f256d7f146
Fonta22 <paufontasp@gmail.com>
2022-10-02 19:36:09 +0200

    Rover mobile version

--- src/components/Rover.jsx
+++ src/components/Rover.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import { formatDate } from '../modules/formatDate';
+import { isMobile } from "react-device-detect";
 
 
 const Rover = () => {
@@ -81,111 +82,224 @@ const Rover = () => {
         getData(sol);
     }, []);
 
-    return (
-        <div>
-            <center>
-                <h1>Rover</h1>
+    if (!isMobile) {
+        return (
+            <div>
+                <center>
+                    <h1>Rover</h1>
+                    <br />
+                    <form onSubmit={ev => {
+                        ev.preventDefault();
+                        const value = ev.target.search.value;
+
+                        if (value === '') return;
+                        setSol(value);
+                        getData(value);
+                    }}>
+                        {/*<button id="arrow-left"></button>*/}
+                        <input className="form-control" type="number" placeholder="Sol" name="search" defaultValue={sol} autoComplete="off" />
+                        <button className="btn btn-outline-secondary" type="submit">Submit</button>
+                        {/*<button id="arrow-right"></button>*/}
+                    </form>
+                </center>
                 <br />
-                <form onSubmit={ev => {
-                    ev.preventDefault();
-                    const value = ev.target.search.value;
+                <center>
+                {/*<div className="hover-content">
+                    <h2>Sol</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere veniam quibusdam earum impedit officiis eum error sequi natus aliquam perferendis accusamus numquam totam vitae, dolore rerum praesentium distinctio repellendus consequuntur?</p>
+                </div>*/}
+                <h2 style={{ marginTop: 20 }}>Photos from <span title="One sol is a day in Mars." className="hover-text">sol</span> <code>{sol}</code>{/*<img src="https://cdn-icons-png.flaticon.com/512/447/447144.png" style={{ height: 25 }} alt="" />*/}</h2>
+                <p style={{ marginBottom: 20 }}>This images were taken by the <b>Curiosity</b> Rover in Mars the day <b>{earthDate}</b>, Sol <b>{sol}</b>.</p>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div className="card" >
+                                    <img className="card-img-top" id="rover-cam-img" src={FHAZ} alt="Card image cap" />
+                                    <div className="card-body">
+                                        <b className="card-text"><i class="bi bi-camera-fill"></i> FHAZ</b>
+                                        <p className="card-text">Front Hazard Avoidance Camera</p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="card" >
+                                    <img className="card-img-top" id="rover-cam-img" src={RHAZ} alt="Card image cap" />
+                                    <div className="card-body">
+                                        <b className="card-text"><i class="bi bi-camera-fill"></i> RHAZ</b>
+                                        <p className="card-text">Rear Hazard Avoidance Camera</p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="card" >
+                                    <img className="card-img-top" id="rover-cam-img" src={MAST} alt="Card image cap" />
+                                    <div className="card-body">
+                                        <b className="card-text"><i class="bi bi-camera-fill"></i> MAST</b>
+                                        <p className="card-text">Mast Camera</p>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="card" >
+                                    <img className="card-img-top" id="rover-cam-img" src={CHEMCAM} alt="Card image cap" />
+                                    <div className="card-body">
+                                        <b className="card-text"><i class="bi bi-camera-fill"></i> CHEMCAM</b>
+                                        <p className="card-text">Chemestry and Camera Complex</p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="card" >
+                                    <img className="card-img-top" id="rover-cam-img" src={MAHLI} alt="Card image cap" />
+                                    <div className="card-body">
+                                        <b className="card-text"><i class="bi bi-camera-fill"></i> MAHLI</b>
+                                        <p className="card-text">Mars Hand Lens Imager</p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                <div className="card" >
+                                    <img className="card-img-top" id="rover-cam-img" src={MARDI} alt="Card image cap" />
+                                    <div className="card-body">
+                                        <b className="card-text"><i class="bi bi-camera-fill"></i> MARDI</b>
+                                        <p className="card-text">Mars Descent Imager</p>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td />
+                            <td>
+                                <div className="card" >
+                                    <img className="card-img-top" id="rover-cam-img" src={NAVCAM} alt="Card image cap" />
+                                    <div className="card-body">
+                                        <b className="card-text"><i class="bi bi-camera-fill"></i> NAVCAM</b>
+                                        <p className="card-text">Navigation Camera</p>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                </center>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <center>
+                    <h1>Rover</h1>
+                    <br />
+                    <form onSubmit={ev => {
+                        ev.preventDefault();
+                        const value = ev.target.search.value;
 
-                    if (value === '') return;
-                    setSol(value);
-                    getData(value);
-                }}>
-                    {/*<button id="arrow-left"></button>*/}
-                    <input className="form-control" type="number" placeholder="Sol" name="search" defaultValue={sol} autoComplete="off" />
-                    <button className="btn btn-outline-secondary" type="submit">Submit</button>
-                    {/*<button id="arrow-right"></button>*/}
-                </form>
-            </center>
-            <br />
-            <center>
-            {/*<div className="hover-content">
-                <h2>Sol</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere veniam quibusdam earum impedit officiis eum error sequi natus aliquam perferendis accusamus numquam totam vitae, dolore rerum praesentium distinctio repellendus consequuntur?</p>
-            </div>*/}
-            <h2 style={{ marginTop: 20 }}>Photos from <span title="One sol is a day in Mars." className="hover-text">sol</span> <code>{sol}</code>{/*<img src="https://cdn-icons-png.flaticon.com/512/447/447144.png" style={{ height: 25 }} alt="" />*/}</h2>
-            <p style={{ marginBottom: 20 }}>This images were taken by the <b>Curiosity</b> Rover in Mars the day <b>{earthDate}</b>, Sol <b>{sol}</b>.</p>
-            <table>
-                <tbody>
-                    <tr>
-                        <td>
-                            <div className="card" >
-                                <img className="card-img-top" id="rover-cam-img" src={FHAZ} alt="Card image cap" />
-                                <div className="card-body">
-                                    <b className="card-text"><i class="bi bi-camera-fill"></i> FHAZ</b>
-                                    <p className="card-text">Front Hazard Avoidance Camera</p>
+                        if (value === '') return;
+                        setSol(value);
+                        getData(value);
+                    }}>
+                        {/*<button id="arrow-left"></button>*/}
+                        <input className="form-control" type="number" placeholder="Sol" name="search" defaultValue={sol} autoComplete="off" />
+                        <button className="btn btn-outline-secondary" type="submit">Submit</button>
+                        {/*<button id="arrow-right"></button>*/}
+                    </form>
+                </center>
+                <br />
+                <center>
+                {/*<div className="hover-content">
+                    <h2>Sol</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere veniam quibusdam earum impedit officiis eum error sequi natus aliquam perferendis accusamus numquam totam vitae, dolore rerum praesentium distinctio repellendus consequuntur?</p>
+                </div>*/}
+                <h2 style={{ marginTop: 20 }}>Photos from <span title="One sol is a day in Mars." className="hover-text">sol</span> <code>{sol}</code>{/*<img src="https://cdn-icons-png.flaticon.com/512/447/447144.png" style={{ height: 25 }} alt="" />*/}</h2>
+                <p style={{ marginBottom: 20 }}>This images were taken by the <b>Curiosity</b> Rover in Mars the day <b>{earthDate}</b>, Sol <b>{sol}</b>.</p>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <div className="card" >
+                                    <img className="card-img-top" id="rover-cam-img" src={FHAZ} alt="Card image cap" />
+                                    <div className="card-body">
+                                        <b className="card-text"><i class="bi bi-camera-fill"></i> FHAZ</b>
+                                        <p className="card-text">Front Hazard Avoidance Camera</p>
+                                    </div>
                                 </div>
-                            </div>
-                        </td>
-                        <td>
-                            <div className="card" >
-                                <img className="card-img-top" id="rover-cam-img" src={RHAZ} alt="Card image cap" />
-                                <div className="card-body">
-                                    <b className="card-text"><i class="bi bi-camera-fill"></i> RHAZ</b>
-                                    <p className="card-text">Rear Hazard Avoidance Camera</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="card" >
+                                    <img className="card-img-top" id="rover-cam-img" src={RHAZ} alt="Card image cap" />
+                                    <div className="card-body">
+                                        <b className="card-text"><i class="bi bi-camera-fill"></i> RHAZ</b>
+                                        <p className="card-text">Rear Hazard Avoidance Camera</p>
+                                    </div>
                                 </div>
-                            </div>
-                        </td>
-                        <td>
-                            <div className="card" >
-                                <img className="card-img-top" id="rover-cam-img" src={MAST} alt="Card image cap" />
-                                <div className="card-body">
-                                    <b className="card-text"><i class="bi bi-camera-fill"></i> MAST</b>
-                                    <p className="card-text">Mast Camera</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="card" >
+                                    <img className="card-img-top" id="rover-cam-img" src={MAST} alt="Card image cap" />
+                                    <div className="card-body">
+                                        <b className="card-text"><i class="bi bi-camera-fill"></i> MAST</b>
+                                        <p className="card-text">Mast Camera</p>
+                                    </div>
                                 </div>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div className="card" >
-                                <img className="card-img-top" id="rover-cam-img" src={CHEMCAM} alt="Card image cap" />
-                                <div className="card-body">
-                                    <b className="card-text"><i class="bi bi-camera-fill"></i> CHEMCAM</b>
-                                    <p className="card-text">Chemestry and Camera Complex</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="card" >
+                                    <img className="card-img-top" id="rover-cam-img" src={CHEMCAM} alt="Card image cap" />
+                                    <div className="card-body">
+                                        <b className="card-text"><i class="bi bi-camera-fill"></i> CHEMCAM</b>
+                                        <p className="card-text">Chemestry and Camera Complex</p>
+                                    </div>
                                 </div>
-                            </div>
-                        </td>
-                        <td>
-                            <div className="card" >
-                                <img className="card-img-top" id="rover-cam-img" src={MAHLI} alt="Card image cap" />
-                                <div className="card-body">
-                                    <b className="card-text"><i class="bi bi-camera-fill"></i> MAHLI</b>
-                                    <p className="card-text">Mars Hand Lens Imager</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="card" >
+                                    <img className="card-img-top" id="rover-cam-img" src={MAHLI} alt="Card image cap" />
+                                    <div className="card-body">
+                                        <b className="card-text"><i class="bi bi-camera-fill"></i> MAHLI</b>
+                                        <p className="card-text">Mars Hand Lens Imager</p>
+                                    </div>
                                 </div>
-                            </div>
-                        </td>
-                        <td>
-                            <div className="card" >
-                                <img className="card-img-top" id="rover-cam-img" src={MARDI} alt="Card image cap" />
-                                <div className="card-body">
-                                    <b className="card-text"><i class="bi bi-camera-fill"></i> MARDI</b>
-                                    <p className="card-text">Mars Descent Imager</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="card" >
+                                    <img className="card-img-top" id="rover-cam-img" src={MARDI} alt="Card image cap" />
+                                    <div className="card-body">
+                                        <b className="card-text"><i class="bi bi-camera-fill"></i> MARDI</b>
+                                        <p className="card-text">Mars Descent Imager</p>
+                                    </div>
                                 </div>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td />
-                        <td>
-                            <div className="card" >
-                                <img className="card-img-top" id="rover-cam-img" src={NAVCAM} alt="Card image cap" />
-                                <div className="card-body">
-                                    <b className="card-text"><i class="bi bi-camera-fill"></i> NAVCAM</b>
-                                    <p className="card-text">Navigation Camera</p>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div className="card" >
+                                    <img className="card-img-top" id="rover-cam-img" src={NAVCAM} alt="Card image cap" />
+                                    <div className="card-body">
+                                        <b className="card-text"><i class="bi bi-camera-fill"></i> NAVCAM</b>
+                                        <p className="card-text">Navigation Camera</p>
+                                    </div>
                                 </div>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            </center>
-        </div>
-    );
-
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                </center>
+            </div>
+        );
+    }
 }
 
 export default Rover;
