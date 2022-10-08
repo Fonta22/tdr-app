0dcdf7df620884b730afd750e7571a4e0cb72e19
Fonta22 <paufontasp@gmail.com>
2022-10-08 12:54:46 +0200

    Add JSDoc to the module daysInMonth.js

--- src/modules/daysInMonth.js
+++ src/modules/daysInMonth.js
@@ -1,3 +1,12 @@
-export function daysInMonth(month, year) {
+/**
+ * Returns the days of the specified month.
+ * 
+ * @param {number} month
+ * @param {number} year
+ * @returns {number} The days of the specified month
+ */
+function daysInMonth(month, year) {
     return new Date(year, month, 0).getDate();
 }
+
+console.log(daysInMonth(7, 2022))
