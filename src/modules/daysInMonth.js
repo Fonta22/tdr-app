ffd2f0bfd881efb7855c9522c3351f9a5a44e6c0
Fonta22 <paufontasp@gmail.com>
2022-10-08 12:55:38 +0200

    Update daysInMonth.js

--- src/modules/daysInMonth.js
+++ src/modules/daysInMonth.js
@@ -5,8 +5,6 @@
  * @param {number} year
  * @returns {number} The days of the specified month
  */
-function daysInMonth(month, year) {
+export function daysInMonth(month, year) {
     return new Date(year, month, 0).getDate();
 }
-
-console.log(daysInMonth(7, 2022))
