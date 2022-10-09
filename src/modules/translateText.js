047bc996972d0b23df60546b0e99850a622d2588
Fonta22 <paufontasp@gmail.com>
2022-10-09 11:21:34 +0200

    Add JSDoc to translateText.js

--- src/modules/translateText.js
+++ src/modules/translateText.js
@@ -1,5 +1,11 @@
 import translate from 'translate';
 
+/**
+ * Using the Google Translate engine translates a text in any language to Catalan.
+ * 
+ * @param {string} text Text in any langauge
+ * @returns {string} Text translated to Catalan
+ */
 export async function translateText(text) {
     translate.engine = 'google';
     const cat = await translate(text, { to: "ca" });
