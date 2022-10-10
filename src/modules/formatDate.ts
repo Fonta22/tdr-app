e165d535bc10a1bdd41d49893ea9fd3109a27466
Fonta22 <paufontasp@gmail.com>
2022-10-10 20:30:51 +0200

    Update formatDate.ts to D/M/YYYY format

--- src/modules/formatDate.ts
+++ src/modules/formatDate.ts
@@ -8,8 +8,9 @@ export function formatDate(date: string): string {
     const dateArr: string[] = date.split('-');
 
     const year: string = dateArr[0];
-    const month: string = dateArr[1];
-    const day: string = Number(dateArr[2]).toString(); // delete first 0
+    // Number().toString() to delete first 0
+    const month: string = Number(dateArr[1]).toString();
+    const day: string = Number(dateArr[2]).toString();
 
     const formatted: string = `${day}/${month}/${year}`;
     return formatted;
