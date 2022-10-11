2698c5a5e28fd8044fa09403c952b38b202dfb5d
Fonta22 <paufontasp@gmail.com>
2022-10-12 00:12:01 +0200

    RM sort_file.py

--- sort_file.py
+++
@@ -1,7 +0,0 @@
-keywords = open('keywords.txt', 'r').read().split('\n') # array
-
-sorted = sorted(keywords)
-content = '\n'.join(sorted)
-
-keywords = open('keywords.txt', 'w')
-keywords.write(content)
